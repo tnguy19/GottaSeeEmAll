ec698e82b061a9e6c7c77e969994907cff35db66
tnguy19 <114105118+tnguy19@users.noreply.github.com>
2024-09-17 16:32:21 -0400

    Update api_keys.js

--- utils/api_keys.js
+++ utils/api_keys.js
@@ -1,1 +1,2 @@
-export const GOOGLE_MAPS_API_KEY = '' //ADD YOUR GOOGLE MAPS API KEY HERE;
+export const GOOGLE_MAPS_API_KEY = '';
+//ADD YOUR GOOGLE MAPS API KEY HERE USING THIS LINK: https://developers.google.com/maps/documentation/javascript/get-api-key 
